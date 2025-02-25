60d435b1191e75b58584eae75c76edccc50dbee0
Amina Zahir <amzahir@c4r10p9.1337.ma>
2025-02-25 21:36:11 +0000

    aa

--- client.c
+++ client.c
@@ -3,28 +3,28 @@
 char c = 0;
 int	send_bit(char c, pid_t s_pid)
 {
+	unsigned char	bit;
 
-	char bit;
-	bit = 0b10000000;
-	
+	bit = 0b10000000;	
 	while (bit)
 	{
 		if (bit & c)
 		{
-			if (kill(s_pid, SIGUSR1) == -1)
-				return (write(2, "Error\n", 6));
+			kill(s_pid, SIGUSR1);// == -1)
+				//return (write(2, "Error\n", 6));
 		}
 		else 
 		{
-			if (kill(s_pid, SIGUSR2) == -1)
-				return (write(2, "Error\n", 6));
+			kill(s_pid, SIGUSR2); //== -1)
+			//	return (write(2, "Error\n", 6));
 		}
-		bit >> 1;
+		//printf("%d\n", bit);
+		bit >>= 1;
 		usleep(100);
 	}
 	return (1);
 }
-int	main(int argc, char **argv)
+int	main(int argc, char **argv)//gfdgdfgdfg ///////////////////// 
 {
 	char	c;
 	int		index;
--- server.c
+++ server.c
@@ -1,37 +1,31 @@
-#include <signal.h> 
-#include <unistd.h>
-#include <sys/types.h>
 #include "minitalk.h"
 
 char c = 0;
 void	sig_handler(int sig_num)
 {
 	static char	bit;
+	static char	c;
 
-	bit = 0b10000000;
-	if (sig_num = SIGUSR1)
-		c = c | bit;	
-	else if (sig_num = SIGUSR2)
-		;
+	if (bit == 0)
+		bit = 0b10000000;
+	if (SIGUSR1)
+		c = c | bit;
 	bit >> 1;
+	if (bit == 0b1)
+	{
+		write(1, &c, 1);
+		bit = 0;
+		c = 0;
+	}	
 }
 int main()
 {
-	printf("server pid %u\n", getpid());
-	int	i;
-
-	i = 8;
+	printf("%u\n", getpid());
 	while (1)
 	{
 		pause();
-		while(i--)
-		{
-			signal(SIGUSR1, sig_handler);
-			signal(SIGUSR2, sig_handler);
-		}
-		write(1, &c, 1);
-		sleep(100);
-		c = 0;
-		c = 0b1000000;
+		signal(SIGUSR1, sig_handler);
+		signal(SIGUSR2, sig_handler);
+		usleep(100);
 	}
 }

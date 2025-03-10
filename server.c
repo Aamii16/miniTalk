f005cbf205ef088032a828c3b849590d2f68d7e3
Amina Zahir <amzahir@c4r10p5.1337.ma>
2025-03-10 01:25:29 +0000

    AAAAAAAAAAAAAAAA

--- server.c
+++ server.c
@@ -12,11 +12,19 @@
 
 #include "minitalk.h"
 
-void	sig_handler(int sig_num)
+void	sig_handler(int sig_num, siginfo_t *info, void *ucontext)
 {
 	static unsigned char	bit;
 	static unsigned char	c;
+	pid_t	client_pid;
 
+	client_pid = info->si_pid;
+	if (client_pid != info->si_pid)
+	{
+		c = 0;
+		bit = 0;
+	}
+	(void)(ucontext);
 	if (bit == 0)
 		bit = 0b10000000;
 	if (sig_num == SIGUSR1)
@@ -28,19 +36,23 @@ void	sig_handler(int sig_num)
 			write(1, "\n", 1);
 		else
 			write(1, &c, 1);
-		usleep(100);
 		c = 0;
+		//usleep(100);
 	}
 }
 
 int	main(void)
 {
+	struct sigaction action;
+
+	action.sa_sigaction = sig_handler;
+	action.sa_flags = SA_SIGINFO;
 	ft_putnbr(getpid());
 	write(1,"\n", 1);
-	signal(SIGUSR1, sig_handler);
-	signal(SIGUSR2, sig_handler);
+	sigaction(SIGUSR1, &action, NULL);
+	sigaction(SIGUSR2, &action, NULL);
 	while (1)
 	{
-		pause();
+		//pause();
 	}
 }
